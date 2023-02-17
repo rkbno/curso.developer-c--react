357c28700050309e2a12344468fecec68d57345c
rk <brenotm10@gmail.com>
2023-02-17 17:12:10 -0300

    estudos

---
+++ javascript/script1.js
@@ -0,0 +1,38 @@
+var idade = 10
+
+console.log(idade)
+if( idade >= 18 && idade <=65 ) {
+    
+}
+else if (idade > 65)  {
+    
+} else {
+    
+}
+    
+    
+switch (idade) {
+    case idade:
+        
+        console.log("Pode")
+        console.log("Qual bebida deseja?")
+        break;
+    case idade: 
+        
+        console.log("Não pode")
+        console.log("Espere mais um ano")
+        break;
+    case idade: 
+        console.log("Não pode")
+        console.log("Espere mais uns anos")
+        break;
+    case idade:
+        
+        console.log("Não pode")
+        console.log("Sua idade não é permitida por conta de problemas") 
+        break;   
+    default :
+        console.log("ERRO 404")    
+}
+
+
