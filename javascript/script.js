d941195818c0d7d40278869cb9211c266253a02a
rk <brenotm10@gmail.com>
2023-02-17 14:12:07 -0300

    estudos

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// alert("Bom dia, Breno")
+var numero = parseInt(prompt("Digite um número!"))
+
+ var quadrado = numero*numero 
+
+alert("O número que você digitou é " +numero+ " ao quadrado ele é " + quadrado)
+
