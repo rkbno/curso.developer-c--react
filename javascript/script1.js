79957ae593669cfa42f186a6ddc10634b49b8874
rk <brenotm10@gmail.com>
2023-02-21 14:25:24 -0300

    estudos

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,35 +1,42 @@
-var idade = 17
-var pode = " "
+// var idade = 17
+// var pode = " "
 
 
-if( idade >= 18 && idade <=65 ) {
-    pode = "Pode"
-}
-else if (idade > 65)  {
-    pode = "não pode"
-} else {
-    pode = "Não pode"
-}
+// if( idade >= 18 && idade <=65 ) {
+//     pode = "Pode"
+// }
+// else if (idade > 65)  {
+//     pode = "não pode"
+// } else {
+//     pode = "Não pode"
+// }
     
-console.log(idade)
-console.log(pode)
+// console.log(idade)
+// console.log(pode)
     
-switch (pode) {
-    case "Pode":
-        console.log("Qual bebida deseja?")
-        break;
-    case "não pode": 
+// switch (pode) {
+//     case "Pode":
+//         console.log("Qual bebida deseja?")
+//         break;
+//     case "não pode": 
         
 
-        console.log("Porquê causa problemas na sua idade")
-        break;
-        case "Não pode": 
+//         console.log("Porquê causa problemas na sua idade")
+//         break;
+//         case "Não pode": 
         
-        console.log("Espere mais um tempo")
-        break;  
-    default :
-        console.log("ERRO 404")    
-        break;
+//         console.log("Espere mais um tempo")
+//         break;  
+//     default :
+//         console.log("ERRO 404")    
+//         break;
+// }
+
+var alunos = ["Arnaldo", "Breno", "Camila", 
+              "Diana", "Eduarda", "Fabio", "Gabriel"]
+
+
+
+for (var aluno of alunos) {
+    console.log(aluno)
 }
-
-
