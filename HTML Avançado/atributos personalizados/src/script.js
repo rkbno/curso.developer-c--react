217f32b25fb5bf8c93337b54e15826eb4740984d
rk <103226779+rkbno@users.noreply.github.com>
2023-03-03 21:17:16 -0300

    estudos

---
+++ HTML Avançado/atributos personalizados/src/script.js
@@ -0,0 +1,35 @@
+/*
+let like = document.getElementById("like")
+
+    like.addEventListener("click", function(){
+
+        
+
+        like.setAttribute("src", "src/imagens/deslike.avif") 
+
+        like.cor = "Vermelho"
+        
+    })
+
+  */
+
+let lista = document.getElementById("lista")
+
+lista.dataset.n = "4"
+
+let num = parseInt(lista.dataset.n)
+
+let conteudo = ""
+
+
+for (let i = 0; i < num; i++){
+    
+    conteudo += "<li>" + i + "</li>"
+
+}
+
+
+
+
+
+// simplificar com boas praticas 
