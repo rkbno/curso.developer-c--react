bbf39fd20467547149f141a34488901dd7111b91
rk <103226779+rkbno@users.noreply.github.com>
2023-04-01 18:17:26 -0300

    fetch

--- javascript avancado/fetch/script.js
+++ javascript avancado/fetch/script.js
@@ -1,21 +1,59 @@
-let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+const inputCep = document.getElementById('cep')
+const btnCep = document.getElementById('btnCep')
+const resultadoCep = document.querySelector('.resultadoCep')
 
-fetch(url)
-    .then((res) =>{
-        
-        return res.json()
+btnCep.addEventListener('click', handleClick)
 
-    }).then((data)=>{
-        console.log(data.low.USD_BRL)
+function handleClick(event) {
+    event.preventDefault()
+    const cep = inputCep.value
+    buscaCep(cep)
+    
+}
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body
     })
-    
-    
-    
-    function converter (){
-        let input = document.getElementById("valor")
-        let valor = input.value
+} 
 
+
+const paragrafo = document.querySelector('.btc')
+
+function precoBtc(){
+    fetch('https://blockchain.info/ticker')
+    .then(response => response.json() )
+    .then(btcJson => {
+        paragrafo.innerHTML = "R$ " + btcJson.BRL.buy
         
-    }
 
-    
+    })
+}
+
+setInterval(precoBtc, 1000 * 30)
+
+precoBtc()
+
+
+const btnProxima = document.querySelector('.proximo')
+const paragrafoPiada = document.querySelector('.piada')
+
+
+function puxarPiada(){
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(r => r.json())
+    .then(piada => {
+        
+        paragrafoPiada.innerText = piada.value
+    })
+}
+
+btnProxima.addEventListener('click', puxarPiada)
+
+
+
+
+
+
